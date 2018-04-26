1accadf415e7904ad5d0d493bdb9e85dd4cd691c
Mikhail Pontus <m.pontus@gmail.com>
2018-04-26 13:09:34 +0300

    Add the video to step details

--- app/src/main/java/com/example/michael/bakingapp/ui/StepDetail/StepDetailFragment.java
+++ app/src/main/java/com/example/michael/bakingapp/ui/StepDetail/StepDetailFragment.java
@@ -1,5 +1,7 @@
 package com.example.michael.bakingapp.ui.StepDetail;
 
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,6 +12,14 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.michael.bakingapp.R;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -19,12 +29,25 @@ public class StepDetailFragment extends Fragment {
     @BindView(R.id.description)
     TextView descriptionView;
 
+    @BindView(R.id.player)
+    PlayerView playerView;
+
+    private SimpleExoPlayer player;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View itemView = inflater.inflate(R.layout.step_detail, container, false);
 
         ButterKnife.bind(this, itemView);
+
+//        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+//        TrackSelection.Factory videoTrackSelectionFactory =
+//                new AdaptiveTrackSelection.Factory(bandwidthMeter);
+        TrackSelector trackSelector = new DefaultTrackSelector();
+
+        player = ExoPlayerFactory.newSimpleInstance(itemView.getContext(), trackSelector);
+        playerView.setPlayer(player);
 
         return itemView;
     }
@@ -34,5 +57,21 @@ public class StepDetailFragment extends Fragment {
     }
 
     public void setVideoUrl(String videoUrl) {
+        Context context = getContext();
+
+        if (context == null) {
+            return;
+        }
+
+
+        DefaultDataSourceFactory dataSourceFactory =
+                new DefaultDataSourceFactory(context, Util.getUserAgent(context, "BakingApp"));
+
+        ExtractorMediaSource mediaSource = new ExtractorMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(Uri.parse(videoUrl));
+
+        player.prepare(mediaSource);
+
+        player.setPlayWhenReady(true);
     }
 }
